83f4fcab6c1a1850bbb81e466c7119a476b61c9c
Paul Klicnik <pklicnik@gmail.com>
2019-05-03 16:25:35 -0400

    Auto download without using form

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # coop-robot
 
 Script to download coop applicant packages in bulk.
+
+## Usage
+
+1. Open `index.js`
+2. Copy and paste contents into browser console
+3. Done!
--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-let buildform = (parameters, action, target) => {
-  var theForm = $(document.createElement("form")).attr("method", "post").attr("action", action).attr("enctype", "multipart/form-data");
+const _showFile = (blob) => {
+  // It is necessary to create a new blob object with mime-type explicitly set
+  // otherwise only Chrome works like it should
+  var newBlob = new Blob([blob], { type: "application/pdf" })
 
-  if (target && typeof target === "string") {
-    theForm.attr("target", target);
+  // IE doesn't allow using a blob object directly as link href
+  // instead it is necessary to use msSaveOrOpenBlob
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(newBlob);
+    return;
   }
-  else if (target && typeof target === "boolean") {
-    theForm.attr("target", '_BLANK' + Math.random() * 100000);
-  }
 
-  $(theForm).insertObject(parameters);
-
-  theForm.append($(document.createElement("input")).attr({
-    type: "hidden",
-    name: "rand",
-    value: Math.floor(Math.random() * 100000)
-  }));
-
-  $(theForm).appendTo("body");
-  return theForm;
+  // For other browsers: 
+  // Create a link pointing to the ObjectURL containing the blob.
+  const data = window.URL.createObjectURL(newBlob);
+  var link = document.createElement('a');
+  link.href = data;
+  link.download = "file.pdf";
+  link.click();
+  setTimeout(function () {
+    // For Firefox it is necessary to delay revoking the ObjectURL
+    window.URL.revokeObjectURL(data);
+  }, 100)
 }
 
-let fetch = () => {
+const _doFetchDocument = (payload) => {
+  let formData = new FormData();
+  formData.append('action', payload.action);
+  formData.append('applicationId', payload.applicationId);
+  formData.append('rand', Math.floor(Math.random() * 100000));
+
+  return fetch("https://waterlooworks.uwaterloo.ca/myAccount/co-op/coopApplications.htm",
+    {
+      body: formData,
+      method: "post"
+    }
+  ).then(response => response.blob())
+    .then(_showFile)
+}
+
+const fetchPackages = async () => {
   const table = document.getElementById("na_jobApplications_employerTableID");
   const links = table.querySelectorAll("tr > td:nth-of-type(3) .dropdown-menu > li:first-of-type > a");
   for (let i = 0; i < links.length; i++) {
@@ -29,13 +47,15 @@ let fetch = () => {
     const startIdx = click.indexOf("{");
     const endIdx = click.indexOf("}");
     const jsonStr = click.substring(startIdx, endIdx + 1).replace(/'/g, '"');
+    let payload;
     try {
-      const payload = JSON.parse(jsonStr);
-      console.log("building", payload.action);
-      buildform(payload, '', '').submit();
-      setTimeout()
-    } catch (err) {
-      console.error("Error parsing", item);
+      payload = JSON.parse(jsonStr);
+    } catch (err) { }
+
+    if (payload) {
+      await _doFetchDocument(payload);
     }
   }
 }
+
+fetchPackages();
